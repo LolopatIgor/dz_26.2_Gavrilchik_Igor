6d3bb49d04231481ef26f9bc97161962d82f850c
Igor Gavrilchik <Lolopat1994@gmail.com>
2024-11-16 17:40:03 +0300

    initial

--- lms/serializers.py
+++ lms/serializers.py
@@ -6,9 +6,14 @@ class LessonSerializer(serializers.ModelSerializer):
         model = Lesson
         fields = '__all__'
 
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
--- users/models.py
+++ users/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from lms.models import Course, Lesson
 
 class User(AbstractUser):
-    username = None  # Убираем поле username
+    username = None
     email = models.EmailField('email address', unique=True)
     phone = models.CharField(max_length=20, blank=True)
     city = models.CharField(max_length=100, blank=True)
@@ -16,3 +15,18 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = (
+        ('cash', 'Наличные'),
+        ('bank_transfer', 'Перевод на счет'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField()
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES)
+
+    def __str__(self):
+        return f"{self.user} paid {self.amount} on {self.payment_date}"
